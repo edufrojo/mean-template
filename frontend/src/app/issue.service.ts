e3aa00d55faf60a5e401dd4f2d686b768ab180f9
edufrojo <edufrojo@gmail.com>
2019-03-07 12:23:51 +0100

    feat(frontend): obtener tareas a través del servicio tarea

--- frontend/src/app/issue.service.ts
+++ frontend/src/app/issue.service.ts
@@ -1,9 +1,19 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
 })
 export class IssueService {
+  uri = 'http://localhost:4000';
+
   constructor(private http: HttpClient) {}
+
+  getIssues() {
+    return this.http.get(`${this.uri}/issues`);
+  }
+
+  getIssueById(id) {
+    return this.http.get(`${this.uri}/issues/${id}`);
+  }
 }
