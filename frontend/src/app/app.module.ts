3d3bf402e1384693250d4357cffdbb5fe74c03bd
edufrojo <edufrojo@gmail.com>
2019-03-07 12:38:46 +0100

    fix(frontend): cargar 'httpclientmodule'

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,20 +2,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+
 import { AppComponent } from './app.component';
-
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
-import { MaterialModule } from './material.module';
-import { IssueService } from './issue.service';
 
 import { ListComponent } from './components/list/list.component';
 import { CreateComponent } from './components/create/create.component';
 import { EditComponent } from './components/edit/edit.component';
 
+import { MaterialModule } from './material.module';
+import { IssueService } from './issue.service';
+
 @NgModule({
   declarations: [AppComponent, ListComponent, CreateComponent, EditComponent],
-  imports: [BrowserModule, AppRoutingModule, MaterialModule, BrowserAnimationsModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    MaterialModule,
+    BrowserAnimationsModule,
+  ],
   providers: [IssueService],
   bootstrap: [AppComponent],
 })
