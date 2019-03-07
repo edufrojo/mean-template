52b769c0ac15353e33aace0a95b1d8e36caff93f
edufrojo <edufrojo@gmail.com>
2019-03-07 12:24:37 +0100

    feat(frontend): añadir tareas a través del servicio tarea

--- frontend/src/app/issue.service.ts
+++ frontend/src/app/issue.service.ts
@@ -16,4 +16,14 @@ export class IssueService {
   getIssueById(id) {
     return this.http.get(`${this.uri}/issues/${id}`);
   }
+
+  addIssue(title, responsible, description, severity) {
+    const issue = {
+      title: title,
+      responsible: responsible,
+      description: description,
+      severity: severity,
+    };
+    return this.http.post(`${this.uri}/issues/add`, issue);
+  }
 }
